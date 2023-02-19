bf988a520b6d67ffc7fe3de015fa768fc68432e6
brnbrd <onvoidmc@gmail.com>
2023-02-18 21:26:57 -0800

    Fix crash, making ND an optional dep once again

--- src/main/java/net/brdle/collectorsreap/compat/ModCompat.java
+++ src/main/java/net/brdle/collectorsreap/compat/ModCompat.java
@@ -3,15 +3,20 @@ package net.brdle.collectorsreap.compat;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.fml.ModList;
 import org.jetbrains.annotations.Nullable;
+import java.util.function.Supplier;
 
 public class ModCompat {
 	@Nullable public static CreativeModeTab getNDTab() {
-		return ifLoaded("nethersdelight", NDCompat.CREATIVE_TAB.get());
+		return ifLoaded("nethersdelight", NDCompat.CREATIVE_TAB);
 	}
 
 	@Nullable public static CreativeModeTab ifLoaded(String modid, CreativeModeTab tab) {
+		return ifLoaded(modid, () -> tab);
+	}
+
+	@Nullable public static CreativeModeTab ifLoaded(String modid, Supplier<CreativeModeTab> tab) {
 		if (ModList.get().isLoaded(modid)) {
-			return tab;
+			return tab.get();
 		} else {
 			return null;
 		}
